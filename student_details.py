157b8e8efffd3115717caff09efa9ff2e2dad20c
AKANKSHAMADALA <106548795+AKANKSHAMADALA@users.noreply.github.com>
2022-07-11 22:17:54 -0700

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -37,7 +37,7 @@ In the above example we have a dictionary in a dictionary with `first-name` and 
 
 Now you are given the function like the below. Fill this function with the necessary code.
 
-This function has to fill in the dictionary with the acquired input and return to the function caller which inturn will print the received dictionary.  
+This function has to fill in the dictionary with the acquired input and return to the function caller which inturn will print the received dictionary.
 
 This function has to return a dictionary with all your details.
 Make sure you fill in the right details.
@@ -45,9 +45,25 @@ Make sure you fill in the right details.
 '''
 
 def my_details():
-  """Return a dictionary with your details in the format as specified in the above instructions"""
-  pass
-
+ x=input('Enter the name of the dict: ')
+d={}
+n=int(input("Enter the number of inputs in the dict: "))
+for j in range(n):
+  d[x]={}
+  for i in range(0,2):
+    key=input("Enter the key name: ")
+    value=input("Enter the value: ")
+  d[x].update({key:value})
+  d.update({'Roll num':'0183'})
+  d.update({'Branch':'cse'})
+  d.update({'Specialization':'core'})
+  d.update({'Email-Id':'makanksh@gitam.in'})
+  d.update({'Replit-Id':'MADALA_Akanksha'})
+  d.update({'GitHub-Id':'akanksha.m'})
+  d.update({'Moodle-Id':'VU21csen0100183'})
+  d.update({'Semester':'2'})
+  d.update({'Year':'1'})
+  return d
 
 # now call your function
 dt = my_details()
